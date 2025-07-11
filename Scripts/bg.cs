21fcd47be4bd8a18252195b5878bf475fc80c250
Evgeniy Rudenko <Zhenya23082003A@gmail.com>
2025-07-11 19:05:26 +0500

    bg animation

---
+++ Scripts/bg.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class BG : ParallaxBackground
+{
+    private double speed = 100;
+
+    public override void _Process(double delta)
+    {
+        ScrollOffset -= new Vector2((float)speed * (float)delta, 0);
+    }
+}
