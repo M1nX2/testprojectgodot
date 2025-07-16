d22887380c7560c633de6e3a793437d3cdc46774
M1nX2 <Zhenya23082003A@gmail.com>
2025-07-16 22:56:00 +0500

    Mushroom rotation

--- Scripts/Mobs/Mushroom.cs
+++ Scripts/Mobs/Mushroom.cs
@@ -12,6 +12,8 @@ public partial class Mushroom : CharacterBody2D
 
     private StateType state;
 
+    private Signals signals;
+
     public StateType State
     {
         get { return state; }
@@ -29,19 +31,32 @@ public partial class Mushroom : CharacterBody2D
                 case StateType.CHASE:
                     ChaseStateStart();
                     break;
+                default:
+                    break;
             }
         }
     }
 
     private Vector2 velocity = Vector2.Zero;
+    private Vector2 playerPosition = Vector2.Zero;
+    private Vector2 direction = Vector2.Zero;
+
+    private Node2D attackDirection;
 
 
-    AnimationPlayer animPlayer;
+    private AnimationPlayer animPlayer;
+    private AnimatedSprite2D sprite;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
         animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        signals = GetNode<Signals>("/root/Signals");
+        attackDirection = GetNode<Node2D>("AttackDirection");
+
+        Callable callable = new(this, "_on_player_position_update");
+        signals.Connect("PlayerPositionUpdate", callable);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -62,6 +77,20 @@ public partial class Mushroom : CharacterBody2D
         base.Velocity = velocity;
     }
 
+    private void _on_player_position_update(Vector2 position)
+    {
+        playerPosition = position;
+
+        if (position.X < Position.X)
+        {
+            velocity.X = Mathf.MoveToward(velocity.X, 0, 100);
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(velocity.X, 0, -100);
+        }
+    }
+
     public void _on_attack_range_body_entered(Node2D body)
     {
         State = StateType.ATTACK;
@@ -76,7 +105,6 @@ public partial class Mushroom : CharacterBody2D
     {
         animPlayer.Play("Attack");
         await ToSignal(animPlayer, "animation_finished");
-
         GetNode<CollisionShape2D>("AttackDirection/AttackRange/CollisionShape2D").Disabled = true;
         State = StateType.CHASE;
         await ToSignal(GetTree().CreateTimer(0.5f), "timeout");
@@ -86,5 +114,16 @@ public partial class Mushroom : CharacterBody2D
     private void ChaseStateStart()
     {
         animPlayer.Play("Run");
+        direction = (playerPosition - this.Position).Normalized();
+        if (direction.X > 0)
+        {
+            sprite.FlipH = false;
+            attackDirection.RotationDegrees = 0;
+        }
+        else
+        {
+            sprite.FlipH = true;
+            attackDirection.RotationDegrees = 180;
+        }
     }
 }
--- Scripts/bg.cs
+++ Scripts/bg.cs
@@ -1,12 +1,12 @@
 using Godot;
 using System;
 
-public partial class BG : ParallaxBackground
+public partial class bg : ParallaxBackground
 {
-    private double speed = 100;
+	private double speed = 100;
 
-    public override void _Process(double delta)
-    {
-        ScrollOffset -= new Vector2((float)speed * (float)delta, 0);
-    }
+	public override void _Process(double delta)
+	{
+		ScrollOffset -= new Vector2((float)speed * (float)delta, 0);
+	}
 }
